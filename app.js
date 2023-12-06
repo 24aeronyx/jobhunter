8e60130dcd101596a9734f07ebd018df6044a11d
24aeronyx <11211017@student.itk.ac.id>
2023-12-07 00:37:12 +0800

    Update port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const allRoutes = require('./routes')
 
-const PORT = 37774
+const PORT = process.env.PORT || 4000
 
 app.use(express.json())
 app.use(allRoutes)
